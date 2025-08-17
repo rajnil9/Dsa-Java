ae869363d10de56aa838146d1058ae087ab8827b
Rajnil Saha <rajnilhere@gmail.com>
2025-08-17 23:23:18 +0530

    adding comment lines

--- cookie_prog/Cookie.java
+++ cookie_prog/Cookie.java
@@ -1,16 +1,23 @@
-package cookie_prog;
+package cookie_prog;  // Package name where this class belongs
 
+// Class definition for Cookie
 public class Cookie {
+    // Private member variable to store the color of the cookie
     private String color;
 
+    // Constructor: called when a new Cookie object is created
+    // It sets the initial color of the cookie
     public Cookie(String c) {
         this.color = c;
     }
+
+    // Getter method: returns the current color of the cookie
     public String getColor() {
         return color;
     }
+
+    // Setter method: allows changing the color of the cookie
     public void setColor(String c) {
         this.color = c;
     }
-    
 }
--- cookie_prog/Main.java
+++ cookie_prog/Main.java
@@ -1,17 +1,25 @@
-package cookie_prog;
+package cookie_prog;   // Package name
 
-import cookie_prog.Cookie;
+import cookie_prog.Cookie;  // Importing the Cookie class from the same package
 
+// Main class to test Cookie objects
 public class Main {
     public static void main(String[] args) {
+        // Create first Cookie object with color "Green"
         Cookie c1 = new Cookie("Green");
+
+        // Create second Cookie object with color "Blue"
         Cookie c2 = new Cookie("Blue");
 
-        System.out.println(c1.getColor());
-        System.out.println(c2.getColor());
+        // Print the colors of both cookies
+        System.out.println(c1.getColor());  // Output: Green
+        System.out.println(c2.getColor());  // Output: Blue
 
+        // Change the color of the first cookie to "Yellow"
         c1.setColor("Yellow");
-        System.out.println(c1.getColor());
-        System.out.println(c2.getColor());
+
+        // Print the updated colors
+        System.out.println(c1.getColor());  // Output: Yellow
+        System.out.println(c2.getColor());  // Output: Blue
     }
 }
